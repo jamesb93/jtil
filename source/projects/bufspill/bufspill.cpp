6d98d01f2d610bf31bf578c0f1634b7e2d12036b
James Bradbury <jamesbradbury93@gmail.com>
2019-08-03 01:33:53 +0100

    check if valid so you dont crash

--- source/projects/bufspill/bufspill.cpp
+++ source/projects/bufspill/bufspill.cpp
@@ -33,26 +33,27 @@ public:
 		MIN_FUNCTION {
             buffer.set(source);
 			buffer_lock<false> b(buffer); // Lock the buffer
-			number m_numframes = b.frame_count();
-			number m_numchans  = b.channel_count();
-			
-			// Array of atoms to be output as the list
-			atoms values(m_numframes + 1);
-			
-			// Loop over the channels
-			for (int i = 0; i < m_numchans; i++) {
-				values[0] = i+1;
+            if (b.valid()) 
+            {
+                number m_numframes = b.frame_count();
+                number m_numchans  = b.channel_count();
+                
+                // Array of atoms to be output as the list
+                atoms values(m_numframes + 1);
+                
+                // Loop over the channels
+                for (int i = 0; i < m_numchans; i++) {
+                    values[0] = i+1;
 
-				// Loop over the samples
-				for (int j = 0; j < m_numframes; j++) {
-					values[j+1] = b.lookup(j, i);
-				}
-				list_out.send(values);
-			}
-			done.send(k_sym_bang);
+                    // Loop over the samples
+                    for (int j = 0; j < m_numframes; j++) {
+                        values[j+1] = b.lookup(j, i);
+                    }
+                    list_out.send(values);
+                }
+                done.send(k_sym_bang);
+            } // If the buffer is valid
 
-			if (b.valid()) // If the buffer is valid
-				cout << " is a valid buffer~" << endl;
 			else
 				cerr << " is not a valid buffer~" << endl; 
 			
